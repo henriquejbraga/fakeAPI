fd471af28d5f14b5c9834955922d303940f54bdf
h.junqueira.braga <henrique.junqueira@engenharia.ufjf.br>
2022-07-25 14:50:43 -0300

    testando retorno do body, com get

--- testesAPI.test.js
+++ testesAPI.test.js
@@ -1,14 +1,67 @@
 const request = require("supertest");
 const API_URL = "https://my-json-server.typicode.com/henriquejbraga/fakeAPI";
 
+const cursos = [
+  {
+    id: 1,
+    titulo: "Entendendo o ES6",
+    url: "https://www.codeprestige.com.br/cursos/es6",
+  },
+  {
+    id: 2,
+    titulo: "Produtividade Máxima com o VS Code",
+    url: "https://www.udemy.com/truques-vscode/",
+  },
+  {
+    id: 3,
+    titulo: "Entendendo o ES7/ES8",
+    url: "https://www.codeprestige.com.br/cursos/es7-8",
+  },
+  {
+    id: 4,
+    titulo: "React 16 Definitivo",
+    url: "https://www.udemy.com/react-16/",
+  },
+];
+
+const alunos = [
+  {
+    id: 1,
+    nome: "Marcela da Silva",
+    curso: 1,
+  },
+  {
+    id: 2,
+    nome: "José Luiz Felipe",
+    curso: 2,
+  },
+  {
+    id: 3,
+    nome: "Paulo Rogério",
+    curso: 3,
+  },
+  {
+    id: 4,
+    nome: "Felipe da Cunha",
+    curso: 1,
+  },
+  {
+    id: 5,
+    nome: "Luiza Cardoso Pereira",
+    curso: 1,
+  },
+];
+
 describe("Faça requisições na API com o método GET", () => {
   it("Faça uma requisição na rota cursos e retorne status 200", async () => {
     const response = await request(API_URL).get("/cursos/");
     expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(cursos);
   });
   it("Faça uma requisição na rota cursos e retorne status 200", async () => {
     const response = await request(API_URL).get("/alunos/");
     expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(alunos);
   });
 });
 
